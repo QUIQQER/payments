b30d36c4f0aa4fb046ae90da9c0dd22c14d79807
Henning Leutz <leutz@pcsg.de>
2014-05-02 17:06:31 +0200

    package payments

---
+++ lib/QUI/Payments/Manager.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * This class contains \QUI\Payments\Manager
+ */
+
+namespace QUI\Payments;
+
+/**
+ * Payment manager
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+
+class Manager
+{
+    /**
+     * Return all active payments
+     *
+     * @return Array
+     */
+    public function getPayments()
+    {
+
+    }
+
+    /**
+     * Return all payments
+     *
+     * @return Array
+     */
+    public function getAllPayments()
+    {
+
+    }
+}
