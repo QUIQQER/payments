92e31a3f9721166b39c60b83349c5a8e5e96c284
Patrick Müller <p.mueller@pcsg.de>
2019-03-26 16:57:45 +0100

    feat: correctly implement RecurringPaymentInterface for Invoice

--- src/QUI/ERP/Accounting/Payments/Methods/Invoice/Payment.php
+++ src/QUI/ERP/Accounting/Payments/Methods/Invoice/Payment.php
@@ -11,13 +11,15 @@ use QUI\ERP\Accounting\Invoice\Invoice;
 use QUI\ERP\Accounting\Invoice\InvoiceTemporary;
 use QUI\ERP\Accounting\Invoice\InvoiceView;
 use QUI\ERP\Accounting\Invoice\Utils\Invoice as InvoiceUtils;
+use QUI\ERP\Accounting\Payments\Types\RecurringPaymentInterface;
+use QUI\ERP\Order\AbstractOrder;
 
 /**
  * Class Payment
  *
  * @package QUI\ERP\Accounting\Payments\Methods\Invoice\Payment
  */
-class Payment extends QUI\ERP\Accounting\Payments\Api\AbstractPayment
+class Payment extends QUI\ERP\Accounting\Payments\Api\AbstractPayment implements RecurringPaymentInterface
 {
     /**
      * @return array|string
@@ -62,16 +64,6 @@ class Payment extends QUI\ERP\Accounting\Payments\Api\AbstractPayment
     }
 
     /**
-     * Does the payment support recurring payments (e.g. for subscriptions)?
-     *
-     * @return bool
-     */
-    public function supportsRecurringPayments()
-    {
-        return true;
-    }
-
-    /**
      * Return an extra invoice text
      *
      * @param Invoice|InvoiceTemporary|InvoiceView $Invoice
@@ -105,5 +97,62 @@ class Payment extends QUI\ERP\Accounting\Payments\Api\AbstractPayment
             'text.invoice.information.for.invoicePayment.pay.date',
             ['date' => $Formatter->format($timeForPayment)]
         );
+    }
+
+    /**
+     * Create a Scubscription from a (temporary) Order
+     *
+     * @param AbstractOrder $Order
+     * @return void
+     */
+    public function createSubscription(AbstractOrder $Order)
+    {
+        // Payment by invoice does not need to create a subscription with any service
+    }
+
+    /**
+     * Capture subscription amount based on an Invoice
+     *
+     * @param Invoice $Invoice
+     * @return void
+     */
+    public function captureSubscription(Invoice $Invoice)
+    {
+        // Invoice payment is manually added
+    }
+
+    /**
+     * Cancel a Subscription
+     *
+     * @param int|string $subscriptionId
+     * @param string $reason (optional) - The reason why the subscription is cancelled
+     * @return void
+     */
+    public function cancelSubscription($subscriptionId, $reason = '')
+    {
+        // Payment by invoice does not need to have/cancel a subscription with any service
+    }
+
+    /**
+     * Can the Subscription of this payment method be edited
+     * regarding essential data like invoice frequency, amount etc.?
+     *
+     * @return bool
+     */
+    public function isSubscriptionEditable()
+    {
+        return true;
+    }
+
+    /**
+     * Check if a Subscription is associated with an order and
+     * return its ID (= identification at the payment method side; e.g. PayPal)
+     *
+     * @param AbstractOrder $Order
+     * @return int|string|false - ID or false of no ID associated
+     */
+    public function getSubscriptionIdByOrder(AbstractOrder $Order)
+    {
+        return false;
     }
 }
